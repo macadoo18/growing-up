58c6f460cde0f6594cbae8d43de62a08b55d0d1f
Mac Hale <machaledesign@gmail.com>
2021-12-12 11:48:36 -0700

    changes api_endpoint

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
-    API_ENDPOINT: 'http://localhost:7000/api',
+    // API_ENDPOINT: 'http://localhost:7000/api',
+    API_ENDPOINT: 'https://sleepy-savannah.herokuapp.com/api',
     TOKEN_KEY: 'growing-up-client-auth-token'
 };
